e766752a1905dbf0e4e28dd3099bed416e654d04
Unknown <AlexOkonor@gmail.com>
2019-01-27 12:11:47 +0300

    Picking items

--- js/game.js
+++ js/game.js
@@ -20,6 +20,7 @@
     );
     var debugTextures = {};
     var cursors;
+    var spaceKey;
 
     function procGenDebugTexture(w,h,name,fill=false,fstyle='#FF3300',sstyle='#ffd900'){
         var bmd = game.make.bitmapData(w,h);
@@ -51,14 +52,17 @@
         //game.load.image('coin', 'assets/coin.png');
         game.load.image('grass', 'assets/grass.png');
         game.load.spritesheet('cat', 'assets/cat.png',16,16);
-        game.load.spritesheet('tree', 'assets/trees.png',16,32);
+        game.load.spritesheet('tree', 'assets/trees.png',16,48);
         game.load.spritesheet('pickup', 'assets/pickup.png',16,16);
         game.load.image('roof', 'assets/roof.png');
         game.load.image('room', 'assets/room.png');
-        game.load.spritesheet('pickup_hud', 'assets/pickup_hud.png',16,16);
+        game.load.spritesheet('pickup_hud', 'assets/pickup_hud2.png',16,16);
         game.load.spritesheet('player', 'assets/player.png',16,24);
 
         game.load.spritesheet('arrows', 'assets/arrow.png', 240, 50);
+
+        game.load.spritesheet('reciept_item', 'assets/recipe_small.png', 10, 10);
+        game.load.image('reciept', 'assets/recipe.png');
         //generate debug textures
         var graphics;
 
@@ -84,10 +88,13 @@
         debugTextures['scFill'] = procGenDebugTexture(game.width,game.height,'',true,'#000000');
 
         cursors = this.input.keyboard.createCursorKeys();
+        spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     }
 
     var ground;
     var player;
+    var reciept;
+    var homeGroup = new Array();
     var homeRoof, homeBase;
 
     var screenArea,
@@ -106,6 +113,9 @@
         items = new Array();
 
     var screenFill;
+    var hud;
+    var hud_text = new Array(4);
+    var pickedItems = [0,0,0,0];
 
     var sleeping;
     var wakingUp;
@@ -130,14 +140,24 @@
         //Sortable
         sortableGroup = game.add.group(game.world, 'sortable'); //sortable is just a name
 
-        homeBase = sortableGroup.create(game.world.centerX, game.world.centerY, 'room');
-        homeBase.anchor.set(0.5);
+        //HOME
+        homePoint = new Phaser.Point(game.world.centerX, game.world.centerY);
+        homeArea = new Phaser.Rectangle(0,0,300,300).centerOn(homePoint);
 
-        homeRoof = sortableGroup.create(game.world.centerX, game.world.centerY, 'roof');
-        homeRoof.anchor.set(0.5);
+        homeBase = sortableGroup.create(homePoint.x, homePoint.y, 'room');
+        homeBase.anchor.set(0.5,98/128);
+
+        homeRoof = sortableGroup.create(homePoint.x, homePoint.y, 'roof');
+        homeRoof.anchor.set(0.5,98/128);
         homeRoof.alpha = 0;
+        homeGroup.push(homeBase);
+        homeGroup.push(homeRoof);
 
-        player = sortableGroup.create(game.world.centerX - 45, game.world.centerY + 10, 'player');
+        player = sortableGroup.create(homePoint.x - 45, homePoint.y + 10, 'player');
+        player.anchor.set(0.5,1);
+
+        reciept = sortableGroup.create(homePoint.x + 17, homePoint.y - 10 + 10, 'reciept_item');
+        reciept.anchor.set(0,1);
 
         //Over screen
         screenFill = game.add.sprite(0,0,debugTextures['scFill']);
@@ -145,21 +165,41 @@
         arrows = game.add.sprite(game.centerX, game.height-50, 'arrows');
         arrows.fixedToCamera = true;
 
+        recieptLarge = game.add.sprite(0,0,'reciept');
+        recieptLarge.fixedToCamera = true;
+        recieptLarge.visible = false;
+
+        hud = game.add.group();
+        hud.fixedToCamera = true;
+        hud.create(10,10,'pickup_hud');
+        hud.create(50,10,'pickup_hud').frame = 1;
+        hud.create(90,10,'pickup_hud').frame = 2;
+        hud.create(130,10,'pickup_hud').frame = 3;
+        hud_text[0] = game.add.text(29, 12, "0", { font: '12px Arial', fill: '#fff' });
+        hud.add(hud_text[0]);
+        hud_text[1] = game.add.text(29+40, 12, "0", { font: '12px Arial', fill: '#fff' });
+        hud.add(hud_text[1]);
+        hud_text[2] = game.add.text(29+40*2, 12, "0", { font: '12px Arial', fill: '#fff' });
+        hud.add(hud_text[2]);
+        hud_text[3] = game.add.text(29+40*3, 12, "0", { font: '12px Arial', fill: '#fff' });
+        hud.add(hud_text[3]);
+        hud.visible = false;
+
         playerFrameSpeed = 8;
 
         player.animations.add('right_idle',[0]);
         player.animations.add('right',[1,1,2,3,4,4,3,2],playerFrameSpeed,true);
-        player.animations.add('right_pick',[0,5,0]);
+        player.animations.add('right_pick',[0,5,0],playerFrameSpeed);
         player.animations.add('up_idle',[6]);
         player.animations.add('up',[7,7,8,9,10,10,9,8],playerFrameSpeed,true);
-        player.animations.add('up_pick',[6,11,6]);
+        player.animations.add('up_pick',[6,11,6],playerFrameSpeed);
         player.animations.add('left_idle',[12]);
         player.animations.add('left',[13,13,14,15,16,16,15,14],playerFrameSpeed,true);
-        player.animations.add('left_pick',[12,17,12]);
+        player.animations.add('left_pick',[12,17,12],playerFrameSpeed);
         player.animations.add('down_idle',[18]);
         player.animations.add('down',[19,19,20,21,22,22,21,20],playerFrameSpeed,true);
-        player.animations.add('down_pick',[18,23,18]);
-        player.animations.add('sleep',[18,25],5,true);
+        player.animations.add('down_pick',[18,23,18],playerFrameSpeed);
+        player.animations.add('sleep',[24,25],5,true);
         
         
         game.physics.enable(player, Phaser.Physics.ARCADE);
@@ -173,9 +213,6 @@
 
         lastRegion.centerOn(player.body.position.x, player.body.position.y);
 
-        homePoint = new Phaser.Point(game.world.centerX, game.world.centerY);
-        homeArea = new Phaser.Rectangle(0,0,300,300).centerOn(homePoint);
-
         debugScreenArea = screenArea.clone().centerOn(game.world.centerX, game.world.centerY);
         debugProtectedArea = protectedArea.clone().centerOn(game.world.centerX, game.world.centerY);
         debugRegenerativeArea = regenerativeArea.clone().centerOn(game.world.centerX, game.world.centerY);
@@ -186,8 +223,20 @@
     var lastDirection = 0;
     var pressedL = false;
     var pressedR = false;
+    var recieptPicked = false;
+    var recieptShow = true;
+    var recieptPoint;
+
+    var picking = false;
+    var pickingTime = 0;
+
     function update() {
+        //PICKING DISABLE
+        if(picking)
+            picking = (game.time.totalElapsedSeconds() - pickingTime < .3);
+
         //MOVEMENT
+        if(!picking)
         if(!sleeping){
             if(cursors.left.isDown){ 
                 player.body.velocity.x = -100;
@@ -243,7 +292,7 @@
             wakingUp *= .99;
 
             screenFill.alpha = Phaser.Math.max(0,(100-wakingUp)/100 );
-            if(wakingUp > 100){
+            if(wakingUp > 50){
                 sleeping = false;
                 screenFill.alpha = 0;
                 arrows.alpha = 0;
@@ -262,7 +311,61 @@
             console.log("Regenerate");
             lastRegion.centerOn(player.body.position.x, player.body.position.y);
             regenerateTrees();
+            if(regeneratedTimes > 10 && !protectedArea.contains(homePoint.x, homePoint.y))
+                lostHome();
         }
+
+        if(Phaser.Math.distanceSq(player.x,player.y,reciept.x,reciept.y+10) < 100){
+            reciept.frame = 1;
+            if(spaceKey.isDown){
+                reciept.destroy();
+                recieptPicked = true;
+                recieptLarge.visible = true;
+                recieptPoint = new Phaser.Point(player.x, player.y);
+            }
+        }else reciept.frame = 0;
+        if(recieptPicked && recieptShow){
+            var dist = Phaser.Math.distanceSq(player.x,player.y,recieptPoint.x,recieptPoint.y);
+            recieptLarge.alpha = Phaser.Math.max(0, (400-dist)/400);
+            if(recieptLarge.alpha <= 0){
+                recieptLarge.destroy();
+                recieptShow = false;
+                hud.visible = true;
+            }
+        }
+
+        //Check for foods picking
+        items.forEach(function(item){
+            if(item.frame%3 != 2){
+                var itemPickRect = item.getBounds();
+                var itemHoverRect = itemPickRect.clone().scale(3,3).centerOn(itemPickRect.centerX, itemPickRect.centerY);
+                var food_id = Math.floor(item.frame/3);
+                if(Phaser.Rectangle.intersects(player.getBounds(),itemPickRect)){
+                    item.frame = food_id*3+2;
+                    pickedItems[food_id]++;
+                    for(var i = 0; i < hud_text.length; i++) hud_text[i].text = pickedItems[i];
+                    picking = true;
+                    pickingTime = game.time.totalElapsedSeconds();
+                    player.body.velocity.x = player.body.velocity.y = 0;
+                    player.animations.stop();
+                    player.animations.play(['left_pick','up_pick','right_pick','down_pick'][lastDirection]);
+                }else if(Phaser.Rectangle.intersects(player.getBounds(),itemHoverRect)){
+                    item.frame = food_id*3+1;
+                }else{item.frame = food_id*3;}
+            }
+        });
+    }
+
+    var isLost = false;
+    function lostHome(){
+        isLost = true;
+        homeGroup.forEach(function(val){val.visible = false;});
+        homePoint.set(0,0);
+        homeArea.centerOn(homePoint.x, homePoint.y);
+    }
+
+    function findHome(){
+        homePoint.set(Phaser.random.frac()*Phaser.Math.PI2);
     }
 
     var regeneratedTimes = 0;
@@ -287,7 +390,7 @@
         trees = newTrees;
         //Remove foods
         var newItems = new Array();
-        trees.forEach(function(val){
+        items.forEach(function(val){
             if(removeCond(val)){
                 val.destroy();
             }else newItems.push(val);
@@ -313,7 +416,9 @@
             //point.multiply(32,32);
             var tree = sortableGroup.create(point.x, point.y, 'tree');
             tree.frame = game.rnd.integerInRange(0,3);
+            tree.anchor.set(0.5,1);
             trees.push(tree);
+
 
             generated++;
         }
@@ -340,6 +445,7 @@
             
             var item = sortableGroup.create(point.x, point.y, 'pickup');
             item.frame = game.rnd.integerInRange(0,3)*3;
+            item.anchor.set(0.5,1);
             items.push(item);
             generated++;
         }
@@ -347,7 +453,7 @@
 
 
     function render () {
-
+        /*
         game.debug.geom(lastRegion,'#0fffff',false);
         game.debug.geom(debugScreenArea,'#0fffff',false);
         game.debug.geom(debugProtectedArea,'#0fffff',false);
@@ -356,6 +462,7 @@
         game.debug.text("Trees: " + trees.length, 10, 30);
         game.debug.text("Waking up: " + wakingUp, 10, 50);
         game.debug.text("Waking dir: " + wakingDirLeft, 10, 70);
+        */
     }
 
 }(Phaser));
